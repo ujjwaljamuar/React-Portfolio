0bdda545fe807827a56d2ae1ec384d73937a31c2
Ujjwal Jamuar <ujjwalj12222@gmail.com>
2024-05-28 12:16:12 +0530

    fixed media queries for mobile devices

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,7 +11,8 @@ const Footer = () => {
         <footer>
             <div>
                 <img
-                    src={"https://avatars.githubusercontent.com/ujjwaljamuar"}
+                    // src={"https://avatars.githubusercontent.com/ujjwaljamuar"}
+                    src={"https://github.com/ujjwaljamuar/Files/assets/56722405/fd9dfb1b-237a-4091-afa8-a255ecbb7426"}
                     alt="Founder"
                 />
 
